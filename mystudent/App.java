3d277961dc59522c6c67f072f4e1e2a741967e21
rjdd14 <72476280+rjdd14@users.noreply.github.com>
2025-02-24 04:28:34 +0000

    Classes and Objects video done

---
+++ mystudent/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String [] args) {
+        
+        Student myStudent = new Student();
+        myStudent.firstName = "Jim";
+        myStudent.lastName = "Halpert";
+        myStudent.major = "Business";
+        myStudent.gpa = 2.3;
+        myStudent.age = 24;
+        myStudent.onProbation = false;
+
+        Student myStudent2 = new Student();
+        myStudent2.firstName = "Pam";
+        myStudent2.lastName = "Beasley";
+        myStudent2.major = "Art";
+        myStudent2.gpa = 2.5;
+        myStudent2.age = 23;
+        myStudent2.onProbation = true;
+
+        System.out.println(myStudent2.firstName);
+    }
+}
